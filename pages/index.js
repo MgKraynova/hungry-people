79b44e025e0540a6184bd5a01cc15b23dca32181
Marina Kraynova <kraynovamg@gmail.com>
2022-02-18 16:23:29 +0300

    add burger menu

--- pages/index.js
+++ pages/index.js
@@ -3,11 +3,14 @@ const secondBurgerLine = document.getElementById('second-line');
 const thirdBurgerLine = document.getElementById('third-line');
 
 const burgerIcon = document.querySelector('.burger-icon');
+const burgerMenu = document.querySelector('.burger__list');
 
 function switchBurgerIcon() {
     firstBurgerLine.classList.toggle('burger-icon__line_animation_first-line');
     secondBurgerLine.classList.toggle('burger-icon__line_animation_second-line');
     thirdBurgerLine.classList.toggle('burger-icon__line_animation_third-line');
+
+    burgerMenu.classList.toggle('burger__list_active');
 }
 
 burgerIcon.addEventListener('click', switchBurgerIcon);
